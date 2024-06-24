4bd3ecd210c7bebe6046003faddec95f89a80253
adityadewhy <adityadewhy@gmail.com>
2024-06-24 23:03:11 +0530

    init

---
+++ index.js
@@ -0,0 +1,15 @@
+const inputField = document.getElementById("input-btn");
+const submitbtn = document.getElementById("submit-btn");
+
+const names = ["ayan"];
+
+submitbtn.addEventListener("click", () => {
+    console.log("btn clicked")
+	const inputValue = inputField.value.trim().toLowerCase();
+
+	if (names.includes(inputValue)) {
+		alert("Current aura balance = -7. don't live in delulu.");
+	} else {
+		alert("Current aura count = +7. Thala for a reason.");
+	}
+});
